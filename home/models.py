df7f14fcc03445f377a61a83977442cf6f594af6
Srikanth <srikanthsri051199@gmail.com>
2022-12-30 15:03:17 +0530

    some changes

--- home/models.py
+++ home/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-
 # Create your models here.
 class Task(models.Model):
     taskTitle = models.CharField(max_length=100)
     taskDesc = models.CharField(max_length=2000)
-    time = models.DateTimeField(auto_now=True)
+    time = models.DateTimeField(auto_created = True)
 
     def __str__(self):
         return self.taskTitle
--- home/views.py
+++ home/views.py
@@ -1,30 +1,18 @@
 from django.shortcuts import render, HttpResponse
 from home.models import Task
-from math import ceil
-# Create your views here.
 def home(request):
     context = {'success': False}
     if request.method == "POST":
         title = request.POST['title']
         desc = request.POST['desc']
-        ins = Task(taskTitle=title, taskDesc=desc)
+        time = request.POST['time']
+        ins = Task(taskTitle=title, taskDesc=desc, time=time)
         ins.save()
+    
         context = {'success': True}
     return render(request, 'index.html', context)
 
 def task(request):
     allTasks = Task.objects.all()
-    # print(allTasks)
-    for item in allTasks:
-        print(item.taskDesc)
     context = {'tasks': allTasks}
     return render(request, 'tasks.html', context)
-
-
-def searchMatch(query, item):
-    if query in item.taskDesc.lower() or query in item.taskTitle.lower():
-        return True
-    else:
-        return False
-
-
